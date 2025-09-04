7cff84246bd8694a50c77e5c92c988e100bb1f45
karamjit-malik <135987100+karamjit-malik@users.noreply.github.com>
2025-09-04 00:19:13 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,6 @@
+"use strict"; // all the js code treated as newer version
+
+let num = null;
+console.log(num)
+console.log(typeof null)
+console.log(typeof undefined)
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const account = 144553
+let accountemail = "karamjit@gmail.com"
+var accountpassword = "12345"
+accountcity = "jaipur"
+
+let accountstate;
+//account = 2 not allowed
+console.log(account);
+accountemail = "hello"
+accountpassword = "121212"
+accountcity = "Bangalore"
+
+console.table([accountemail , accountcity , accountpassword , accountstate]);
