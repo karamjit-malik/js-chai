958456c65f467e87199aa1f5822e7d43d4d30bfc
karamjit-malik <135987100+karamjit-malik@users.noreply.github.com>
2025-09-04 11:54:32 +0000

    summary of datatypes

---
+++ datatype_summary.js
@@ -0,0 +1,18 @@
+//const num = Symbol("123")
+//const num2 = Symbol("123")
+//console.log(num === num2);
+
+
+const mufun = function()
+{
+    console.log("hello")
+}
+mufun();
+
+
+let obj = {
+    name : "Karam",
+    age : 20
+};
+
+console.log(typeof mufun)
