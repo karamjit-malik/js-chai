485ea40107dcc1dd966a40e5ece76ec762866584
karamjit-malik <135987100+karamjit-malik@users.noreply.github.com>
2025-09-04 13:32:13 +0000

    thek

--- 01_basics/datatype_summary.js
+++ 01_basics/datatype_summary.js
@@ -3,11 +3,11 @@
 //console.log(num === num2);
 
 
-const mufun = function()
-{
-    console.log("hello")
-}
-mufun();
+//const mufun = function()
+//{
+//   console.log("hello")
+//}
+//mufun();
 
 
 let obj = {
@@ -16,3 +16,28 @@ let obj = {
 };
 
 console.log(typeof mufun)
+
+//let address = "Karam ka ghar"
+//let add2 = address
+//console.log(add2);
+//add2 = "Mera ghar"
+//console.log(add2);
+
+const obj1 = {
+    name : "Karam",
+    age : 20
+};
+const obj2 = {
+    name : "Karam",
+    age : 20
+};
+//console.log(obj1 === obj2)
+
+const ob1 = {
+    name : "Karam",
+    age: 21
+};
+const ob2 = ob1
+//console.log(ob1 === ob2)
+ob2.age = 25
+console.log(ob1.age)
