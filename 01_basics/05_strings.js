11c22005080d9e991487d2ec7dd3d4a3b510f103
karamjit-malik <135987100+karamjit-malik@users.noreply.github.com>
2025-09-04 16:45:24 +0000

    string functions

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,25 @@
+//let name = "Karam"
+//let count = 50
+//console.log(name + count);
+//console.log(`Name : ${name} , count : ${count}`)
+
+//const str = new String('Hello')
+//console.log(str)
+//console.log(str.length)
+//console.log(str.charAt(2))
+//console.log(str.substrign(0,3))
+//console.log(str.slice(0,-2))
+
+
+//let str = `  Karam `;
+//console.log(str)
+//console.log(str.trim())
+//console.log(str.trimStart())
+//console.log(str.trimEnd())
+
+let str = `Karamjit Malik`
+str = str.replace('jit','')
+// Simply cant do the thing need to store it somewhere before use
+console.log(str);
+console.log(str.includes('Karam'))
+console.log(str.split(' '));
