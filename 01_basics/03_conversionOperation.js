196af0c86506a83ae0bef7fa94c58a7e02575450
karamjit-malik <135987100+karamjit-malik@users.noreply.github.com>
2025-09-04 11:06:29 +0000

    conversions

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -4,5 +4,16 @@
 //console.log(typeof num)
 //console.log(num)
 
-let booe = -1;
-console.log(Boolean(booe));
+//let booe = -1;
+//console.log(Boolean(booe));
+
+let str1 = "hello"
+let str2 = "karam"
+let str3 = str1+str2
+console.log(str3)
+
+//console.log("1"+2)
+//console.log(1+"2")
+//console.log(1+2+"2")
+
+console.log(+true)
