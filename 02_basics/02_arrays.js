b00cf015f300c7ca5ec1c0c33b0aae46b3f7e151
karamjit-malik <135987100+karamjit-malik@users.noreply.github.com>
2025-09-05 23:05:47 +0000

    array basics 2

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,23 @@
+const marvel = ['thor' , 'Ironman' , 'Hulk']
+const dc = ['Superman' , 'Flash' , 'Batman']
+//marvel.push(dc)
+//console.log(marvel)
+
+//const newa = [...marvel , ...dc]
+//console.log(newa)
+
+//let array = [1,2,3,[4,5],[6,[7,8]]]
+//array.flat(Infinity)
+//console.log(array)
+//array = array.flat(Infinity)
+//console.log(array)
+
+//let str = 'Karam'
+//let arr = Array.from(str)
+//console.log(arr)
+//console.log(Array.from({name : `Karamjit`}))
+
+let s1=100
+let s2=200
+let s3=300
+console.log(Array.of(s1,s2,s3))
