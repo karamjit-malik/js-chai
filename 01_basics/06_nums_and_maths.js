6d4b7684e7a7c5d79a5aaa0c56743bc85869c9ee
karamjit-malik <135987100+karamjit-malik@users.noreply.github.com>
2025-09-05 08:13:33 +0000

    Math and num

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,22 @@
+const nums = 40000000
+//console.log(nums);
+
+//console.log(nums.toExponential())
+
+//console.log(nums.toFixed(2))
+
+//console.log(nums.toLocaleString())
+
+//console.log(typeof nums)
+
+//console.log(nums.valueOf());
+
+//console.log(nums.toString().length)
+
+//const newnum = 23.8966
+//console.log(newnum.toPrecision(2))
+
+//console.log(Math.exp(1));
+
+
+console.log(Math.random())
