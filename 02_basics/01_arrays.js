c2458037c6b812f734c5bce4f5baee21532c7de5
karamjit-malik <135987100+karamjit-malik@users.noreply.github.com>
2025-09-05 21:51:08 +0000

    array basics 1

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,36 @@
+//let arr = [1,2,3,4,5]
+//console.log(arr);
+
+//let a = new Array(1,2,3,4,5)
+//console.log(a)
+
+//console.log(a.at(2))
+
+//a.push(6)
+//console.log(a)
+
+//a.pop()
+//console.log(a)
+
+//a.unshift(0)
+//console.log(a)
+
+//a.shift()
+//console.log(a)
+
+//a.join()
+//console.log(a)
+
+//let str = a.join(`--`)
+//console.log(str)
+// need to store it , it doesnt convert the original array to string 
+
+
+//console.log(a.slice(0,3))
+//a.splice(0,1)
+//console.log(a)
+//let a2 = [6,7,8,9]
+//let a3 = [];
+//a3 = a3.concat(a,a2) 
+//console.log(a3)
+
