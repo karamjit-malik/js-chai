eb63603b050f80a773033167d2b96d91394c48e8
karamjit-malik <135987100+karamjit-malik@users.noreply.github.com>
2025-09-06 22:40:50 +0000

    if else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,11 @@
+//if
+// vari = false
+// if(vari)
+//     console.log('ok')
+// else
+//     console.log('not ok')
+
+if(2 === '2')
+    console.log('equal hein')
+if(2 == '2')
+    console.log('equal')
