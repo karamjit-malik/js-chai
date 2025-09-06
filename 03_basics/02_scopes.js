25e8d94a44dfa2e8b8bf30240ba47dcd53fcfa57
karamjit-malik <135987100+karamjit-malik@users.noreply.github.com>
2025-09-06 11:52:55 +0000

    ok

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,36 @@
+// if(true)
+// {
+//     let a = 10
+//     const b = 20
+//     var c = 30
+// }
+
+//console.log(a)
+//console.log(b)
+// console.log(c)
+
+// function one()
+// {
+//     const name = 'karam'
+//     console.log(name)
+//     function two()
+//     {
+//         const website = 'yt'
+//         console.log(name)
+//     }
+//     console.log(website)
+//     two()
+// }
+// one()
+
+
+console.log(add(1))
+function add(num)
+{
+    return num+1;
+}
+console.log(two)
+const two = function(num)
+{
+    return num+2
+}
