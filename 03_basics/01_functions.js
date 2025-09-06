76df36580927d99f6b465bc014aa771962da644a
karamjit-malik <135987100+karamjit-malik@users.noreply.github.com>
2025-09-06 02:35:43 +0000

    thek

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,26 @@
+function name()
+{
+    console.log('Karam')
+}
+// name()
+
+// let res = name()
+// console.log(res)
+
+// function add(a = 2,b = 3)
+// {
+//     return a+b;
+// }
+// console.log(add())
+// console.log(add(1,2))
+
+// let addition = function(val1,...num1)
+// {
+//     return num1;
+// }
+// console.log(addition(1,2,3,4,5))
+
+const obj = {}
+obj.name = 'karam'
+obj.age = 20
+console.log(obj.hasOwnProperty('day'))
